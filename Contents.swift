4d0a85b618152de4e34211a369de7b7512f1531c
Shem <Shem@Hidden-MacBook-Air.local>
2017-03-06 16:26:02 -0800

    func

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func addNumber(number: Int){
+    print ("Hello \(number)");
+}
+
+addNumber(number: 55);
